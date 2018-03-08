b0260caaf811080f35e5778aa257fd44a77d9740
sylhare <sylhareng@gmail.com>
2018-03-08 15:29:05 -0500

    Initial commit

---
+++ charpy/app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def chart():
+    labels = ["January", "February", "March", "April", "May", "June", "July", "August"]
+    values = [10, 9, 8, 7, 6, 4, 7, 8]
+    colors = ["#F7464A", "#46BFBD", "#FDB45C", "#FEDCBA", "#ABCDEF", "#DDDDDD", "#ABCABC"]
+    return render_template('chart.html', values=values, labels=labels, set=zip(values, labels, colors))
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5001)
