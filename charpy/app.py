80c6dcad52a727e626e4815b44a2f2b956e785b8
sylhare <sylhareng@gmail.com>
2018-03-09 14:49:10 -0500

    Minor fix on app.py

--- charpy/app.py
+++ charpy/app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 def chart():
     labels = ["January", "February", "March", "April", "May", "June", "July", "August"]
     values = [10, 9, 8, 7, 6, 4, 7, 8]
-    colors = ["#F7464A", "#46BFBD", "#FDB45C", "#FEDCBA", "#ABCDEF", "#DDDDDD", "#ABCABC"]
+    colors = ["#F7464A", "#46BFBD", "#FDB45C", "#FEDCBA", "#ABCDEF", "#DDDDDD", "#ABCABC", "#BCAFED"]
     return render_template('chart.html', values=values, labels=labels, set=zip(values, labels, colors))
 
 
