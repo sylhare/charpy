c97ef11da27a83fc666f3e5912b5310e00592ef0
sylhare <sylhareng@gmail.com>
2018-04-04 09:44:57 -0400

    Update data tests

--- charpy/cruncher.py
+++ charpy/cruncher.py
@@ -1,7 +1,7 @@
 import os
-import datetime
-from dateutil.parser import parse
 import pandas as pd
+import re
+from dateutil.parser import parse
 from charpy import DATA_PATH
 
 
@@ -30,31 +30,61 @@ class Orc(object):
 
         self.df = pd.read_csv(path, **criteria)
 
-    def format_date(self, column, formatting="%d/%m/%Y"):
+    # TODO check the formatting is a correct time formatting
+    def format_column_date(self, column, formatting="%d/%m/%Y", dayfirst=True):
         """
         Format a column into a date format
 
+        :param dayfirst: True for the parsing method
         :param formatting:
         :param column:
         :return:
         """
+        c = self.__check_column(column)
+        if c:
+
+            self.df[c] = list(map(lambda x: parse(x, dayfirst=dayfirst).date().strftime(formatting), self.df[c]))
+
+    def format_column_list(self, column, regex=r'  +'):
+        """
+
+        :param c: column to apply
+        :param regex: by default more than two spaces
+        :return:
+        """
+
+        c = self.__check_column(column)
+        if c:
+
+            self.df[c] = list(map(lambda x: re.split(regex, x), self.df[c]))
+
+    def __check_column(self, column):
+        """
+        Column can be a number of a name
+
+        :param column:
+        :return: the column name
+        """
+
         try:
             if int == type(column):
-                c = self.df.columns[column]
-            elif str == type(column):
-                c = column
+                c_name = self.df.columns[column]
+            elif column in self.df:
+                c_name = column
             else:
-                raise TypeError("column should be the number or the name of the column")
-
-            self.df[c] = list(map(lambda x: parse(x).date().strftime(formatting), self.df[c]))
+                raise TypeError("Error: column should be the number or the name of the column")
 
         except TypeError as error:
             print(error)
+            c_name = False
+
+        return c_name
 
 
 if __name__ == "__main__":  # pragma: no cover
     s = Orc(os.path.join(DATA_PATH, "pcbanking.csv"))
     print(s.df)
-    s.format_date("date")
+    s.format_column_date("date")
     print(s.df)
-
+    for value in s.df['label']:
+        print(re.split(r'  +', value))
--- tests/test_orc.py
+++ tests/test_orc.py
@@ -1,26 +1,41 @@
 import os
 import unittest
-from charpy import DATA_PATH, cruncher
+from charpy import DATA_PATH
+from charpy.cruncher import Orc
 from tests import *
 
 
 class TestConverter(unittest.TestCase):
 
+    def setUp(self):
+        self.file = Orc(os.path.join(DATA_PATH, "demo.csv"), sep=',', header=0)
+        self.semicolon = Orc(os.path.join(DATA_PATH, "demo-semicolon.csv"), header=0)
+
     def test_is_csv_delimiter_agnostic(self):
         """ The csv file can be opened however the delimiter """
-        preset = cruncher.Orc(os.path.join(DATA_PATH, "demo.csv"), sep=',').df.columns.tolist()
-        comma = cruncher.Orc(os.path.join(DATA_PATH, "demo.csv")).df.columns.tolist()
-        semicolon = cruncher.Orc(os.path.join(DATA_PATH, "demo-semicolon.csv")).df.columns.tolist()
+        preset = self.file.df.columns.tolist()
+        comma = Orc(os.path.join(DATA_PATH, "demo.csv"), header=0).df.columns.tolist()
+        semicolon = self.semicolon.df.columns.tolist()
         self.assertEqual(semicolon, comma)
         self.assertEqual(preset, comma)
 
-    def test_is_date_formatted(self):
-        file = cruncher.Orc(os.path.join(DATA_PATH, "demo.csv"), sep=',', header=0)
-        file.format_date(0)
-        semicolon = cruncher.Orc(os.path.join(DATA_PATH, "demo-semicolon.csv"), header=0)
-        semicolon.format_date('Month')
-        self.assertEqual(file.df['Month'].all(), semicolon.df[semicolon.df.columns[0]].all())
-        self.assertEqual(file.df['Month'].tolist(), TEST_MONTH_FORMATTED)
+    def test_is_date_formatted_default(self):
+        """ The date should be formatted in month """
+        self.file.format_column_date(0)
+        self.semicolon.format_column_date('Month')
+        self.assertEqual(self.file.df['Month'].all(), self.semicolon.df[self.semicolon.df.columns[0]].all())
+
+    def test_is_date_formatted_month(self):
+        """ Change data format to default then back to Month and make sure it is still ok """
+        self.file.format_column_date(0)
+        self.file.format_column_date(0, formatting='%B')
+        self.assertEqual(self.file.df['Month'].tolist(), TEST_DATA_COLUMN[0])
+
+    def test_is_list_formatted(self):
+        """ test column if formatted to list """
+        self.file.format_column_list(2, regex=r'#')
+        self.assertTrue(type(self.file.df['Colors'][0]) == list)
+        self.assertTrue(len(self.file.df['Colors'][0]) == 2)
 
 
 if __name__ == "__main__":
