56b97b6cfe536749aa93793566c307e68b910e6c
sylhare <sylhareng@gmail.com>
2018-04-20 11:39:58 -0400

    Update error template

--- charpy/flask/blueprints/default.py
+++ charpy/flask/blueprints/default.py
@@ -5,4 +5,9 @@ bp = Blueprint('default', __name__, template_folder='templates')
 
 @bp.app_errorhandler(404)
 def page_not_found(error):
-    return render_template('layout/404.html', error=404, message_1="THE PAGE", message_2="WAS NOT FOUND"), 404
+    return render_template('layout/error.html', color="#2B547E", error=404, message_1="THE PAGE", message_2="WAS NOT FOUND"), 404
+
+
+@bp.app_errorhandler(501)
+def page_not_found(error):
+    return render_template('layout/error.html', color="#006600", error=501, message_1="FEATURE", message_2="NOT IMPLEMENTED"), 501
